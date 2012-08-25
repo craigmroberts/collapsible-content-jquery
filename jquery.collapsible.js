4357b7b86413cd684746802d796f3e775c09ba1f
Craig M Roberts <craig.m.roberts01@gmail.com>
2012-08-25 10:55:30 +0100

    New features and refined code.
    I have been thinking of new ways to improve this little script. Here is
    whats new;
    
    - Code has been refined and is now a re-usable plugin.
    - Indicators - Add status indicators to the label (+|-, open | close ).
    - Indicators position - Chose where in the head you want them to appear
    (left or right).
    - Auto scroll - If set to true this feature will autoscroll the group
    headers to keep the clicked header in view.
    - Auto scroll speed - Set the scroll speed.
    
    Enjoy!

--- jquery.collapsible.js
+++ jquery.collapsible.js
@@ -1,51 +1,102 @@
-var Collapsible = Collapsible || {};
+jQuery.fn.collapsible = function(options) {
+    var Collapsible = Collapsible || {};
+    alert
+    var defaults = {
+        safe: false,
+        event: 'click',
+        group_name: ".group",
+        indicators: false,
+        indi_names: '-|+',
+        indi_pos: 'right',
+        auto_scroll: false,
+        auto_scroll_speed: 600
+    };
+    var options = $.extend(defaults, options);
 
-Collapsible.run = function(noJS) {
-    var i = 0;
-    for (i; i < $('.group').length; i++) {
-        $('.group:eq(' + i + ')').addClass('grp_' + i + '');
-    }
+    var $indi_pos = options.indi_pos;
+    var $indi_label = options.indi_names.split('|');
 
-    var hiddenEl = $('.collapse label');
-    for (i = 0; i < hiddenEl.length; i++) {
-        var lbl = $('.collapse label:eq(' + i + ')');
-        if (noJS) {
-            if (!lbl.hasClass('open')) {
-                if (!lbl.hasClass('hide')) {
-                    lbl.next().addClass('hide');
+    Collapsible.indicators = function($el) {
+        if ($el !== undefined) {
+            $el.toggleClass('open');
+            $el.next().toggleClass('hide');
+            if (options.indicators === true) {
+                if ($el.children().html() == $indi_label[0]) {
+                    $el.children().html($indi_label[1]);
                 }
+                else {
+                    $el.children().html($indi_label[0]);
+                };
+            };
+        } else {
+            if (options.indicators === true) {
+                if ($indi_pos == 'left') {
+                    $indi_pos = 'style="float:left"';
+                }
+                return '<span ' + $indi_pos + '>' + $indi_label[1] + '</span>';
+            };
+        };
+    };
+
+    Collapsible.inGroup = function($el) {
+        $el = $el.parent().parent();
+        if ($el.hasClass(options.group_name.substring(1))) {
+            var grp = $('.' + $el.attr('class').split(' ')[1] + ' .open');
+
+            grp.next().toggleClass('hide');
+            grp.children().text($indi_label[1]);
+            grp.toggleClass('open');
+        };
+    };
+
+    for (i = 0; i < $(options.group_name).length; i++) {
+        var $thisGroup = options.group_name + ':eq(' + i + ')';
+
+        $($thisGroup).addClass('grp_' + i + '');
+
+        for (ii = 0; ii < $(options.group_name + ':eq(' + i + ') label').length; ii++) {
+            var lbl = $($thisGroup + ' label:eq(' + ii + ')');
+       
+            if (options.safe === true) {
+                if (!lbl.hasClass('open')) {
+                    if (!lbl.hasClass('hide')) {
+                        lbl.next().addClass('hide');
+                    };
+                };
+            };
+            lbl.append(Collapsible.indicators());
+        };
+    };
+
+
+    this.bind(options.event, function(e) {
+        var $el = $(e.target);
+
+        if ($el.parent().children().length > 1) {
+            if ($el.hasClass('open')) {
+                Collapsible.indicators($el);
             }
+            else {
+                Collapsible.inGroup($el);
+                Collapsible.indicators($el);
+            };
         }
-    }
-    $('.collapse label').bind('click', Collapsible.collapse);
+        if (options.auto_scroll === true) {
+            var scroll_top = $el.position().top;
+            $("html, body").animate({
+                scrollTop: scroll_top
+            }, options.auto_scroll_speed);
+        };
+    });
 };
 
-Collapsible.toggle = function($el) {
-    $el.toggleClass('open');
-    $el.next().toggleClass('hide');
-};
-
-Collapsible.inGroup = function($el) {
-    $el = $el.parent().parent();
-    if ($el.hasClass('group')) {
-        var grp = $el.attr('class').split(' ')[1];
-
-        $('.' + grp + ' .open').next().toggleClass('hide');
-        $('.' + grp + ' .open').toggleClass('open');
-    }
-};
-
-Collapsible.collapse = function(e) {
-    var $el = $(e.target);
-
-    if ($el.parent().children().length > 1) {
-
-        if ($el.hasClass('open')) {
-            Collapsible.toggle($el);
-        }
-        else {
-            Collapsible.inGroup($el);
-            Collapsible.toggle($el);
-        }
-    }
-};
+jQuery('label').collapsible({
+    safe: true,
+    event: 'click',
+    group_name: '.group',
+    indicators: true,
+    indi_names: 'open|close',
+    indi_pos: 'right',
+    auto_scroll: true,
+    auto_scroll_speed: 700
+});
