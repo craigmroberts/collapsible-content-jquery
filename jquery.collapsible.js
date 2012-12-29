1c37d1eec25058e7cdfd9074298124b70342ddaf
Craig M Roberts <craig.m.roberts01@gmail.com>
2012-12-29 16:09:06 +0000

    Code revisted
    The code has been improved

--- jquery.collapsible.js
+++ jquery.collapsible.js
@@ -4,7 +4,6 @@ jQuery.fn.collapsible = function(options) {
     var defaults = {
         safe: false,
         event: 'click',
-        group_name: ".group",
         indicators: false,
         indi_names: '-|+',
         indi_pos: 'right',
@@ -12,7 +11,7 @@ jQuery.fn.collapsible = function(options) {
         auto_scroll_speed: 600
     };
     var options = $.extend(defaults, options);
-
+    var $grp_name = '.group';
     var $indi_pos = options.indi_pos;
     var $indi_label = options.indi_names.split('|');
 
@@ -28,19 +27,13 @@ jQuery.fn.collapsible = function(options) {
                     $el.children().html($indi_label[0]);
                 };
             };
-        } else {
-            if (options.indicators === true) {
-                if ($indi_pos == 'left') {
-                    $indi_pos = 'style="float:left"';
-                }
-                return '<span ' + $indi_pos + '>' + $indi_label[1] + '</span>';
-            };
         };
     };
 
+
     Collapsible.inGroup = function($el) {
         $el = $el.parent().parent();
-        if ($el.hasClass(options.group_name.substring(1))) {
+        if ($el.hasClass($grp_name.substring(1))) {
             var grp = $('.' + $el.attr('class').split(' ')[1] + ' .open');
 
             grp.next().toggleClass('hide');
@@ -49,54 +42,68 @@ jQuery.fn.collapsible = function(options) {
         };
     };
 
-    for (i = 0; i < $(options.group_name).length; i++) {
-        var $thisGroup = options.group_name + ':eq(' + i + ')';
 
-        $($thisGroup).addClass('grp_' + i + '');
 
-        for (ii = 0; ii < $(options.group_name + ':eq(' + i + ') label').length; ii++) {
-            var lbl = $($thisGroup + ' label:eq(' + ii + ')');
-       
-            if (options.safe === true) {
-                if (!lbl.hasClass('open')) {
-                    if (!lbl.hasClass('hide')) {
-                        lbl.next().addClass('hide');
-                    };
+    for (i = 0; i < $('.collapse li#head').length; i++) {
+        var $label = $('.collapse li#head:eq(' + i + ')');
+
+        if (options.safe === true) {
+            if (!$label.hasClass('open')) {
+                if (!$label.hasClass('hide')) {
+                    $label.next().addClass('hide');
                 };
             };
-            lbl.append(Collapsible.indicators());
         };
-    };
 
 
-    this.bind(options.event, function(e) {
-        var $el = $(e.target);
 
-        if ($el.parent().children().length > 1) {
-            if ($el.hasClass('open')) {
-                Collapsible.indicators($el);
+        if (options.indicators === true) {
+            if ($indi_pos == 'left') {
+                $indi_pos = 'style="float:left"';
             }
-            else {
-                Collapsible.inGroup($el);
-                Collapsible.indicators($el);
+
+
+            if ($label.hasClass('open')) {
+                $label.append('<span ' + $indi_pos + '>' + $indi_label[0] + '</span>');
+            } else {
+                $label.append('<span ' + $indi_pos + '>' + $indi_label[1] + '</span>');
             };
-        }
-        if (options.auto_scroll === true) {
-            var scroll_top = $el.position().top;
-            $("html, body").animate({
-                scrollTop: scroll_top
-            }, options.auto_scroll_speed);
+
+
+
+
         };
-    });
+
 };
 
-jQuery('label').collapsible({
-    safe: true,
-    event: 'click',
-    group_name: '.group',
-    indicators: true,
-    indi_names: 'open|close',
-    indi_pos: 'right',
-    auto_scroll: true,
-    auto_scroll_speed: 700
+
+
+for (i = 0; i < $($grp_name).length; i++) {
+    var $thisGroup = $grp_name + ':eq(' + i + ')';
+
+    $($thisGroup).addClass('grp_' + i + '');
+};
+
+
+$('.collapse li#head').bind(options.event, function(e) {
+
+    var $el = $(e.target);
+
+    if ($el.parent().children().length > 1) {
+
+        if ($el.hasClass('open')) {
+            Collapsible.indicators($el);
+        }
+        else {
+            Collapsible.inGroup($el);
+            Collapsible.indicators($el);
+        };
+    }
+    if (options.auto_scroll === true) {
+        var scroll_top = $el.position().top;
+        $("html, body").animate({
+            scrollTop: scroll_top
+        }, options.auto_scroll_speed);
+    };
 });
+};
