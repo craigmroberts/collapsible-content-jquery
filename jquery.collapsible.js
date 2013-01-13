71309b577d23b8172928b00599f4b98d98d70819
Craig M Roberts <craig.m.roberts01@gmail.com>
2013-01-13 16:33:17 +0000

    Tidier and Readable

--- jquery.collapsible.js
+++ jquery.collapsible.js
@@ -1,109 +1,115 @@
-jQuery.fn.collapsible = function(options) {
-    var Collapsible = Collapsible || {};
-    alert
-    var defaults = {
-        safe: false,
-        event: 'click',
-        indicators: false,
-        indi_names: '-|+',
-        indi_pos: 'right',
-        auto_scroll: false,
-        auto_scroll_speed: 600
+$.fn.collapsible = function (options) {
+  var Collapsible = Collapsible || {};
+
+  var options = $.extend({
+    non_js_support: false,
+    event_trigger: 'click',
+    indicators: false,
+    indicators_labels: '-|+',
+    auto_scroll: false,
+    auto_scroll_speed: 600,
+    rounded: false
+  }, options);
+
+  var group = '.group';
+  var indicators_labels = options.indicators_labels.split('|');
+
+  Collapsible.indicators = function (el) {
+    if (el !== undefined) {
+      el.toggleClass('open');
+      el.next().toggleClass('hide');
+      if (options.indicators === true) {
+        var lbl;
+        if (el.children().html() == indicators_labels[0]) {
+          lbl = indicators_labels[1]
+        } else {
+          lbl = indicators_labels[0]
+        };
+        el.children().html(lbl);
+      };
     };
-    var options = $.extend(defaults, options);
-    var $grp_name = '.group';
-    var $indi_pos = options.indi_pos;
-    var $indi_label = options.indi_names.split('|');
+  };
 
-    Collapsible.indicators = function($el) {
-        if ($el !== undefined) {
-            $el.toggleClass('open');
-            $el.next().toggleClass('hide');
-            if (options.indicators === true) {
-                if ($el.children().html() == $indi_label[0]) {
-                    $el.children().html($indi_label[1]);
-                }
-                else {
-                    $el.children().html($indi_label[0]);
-                };
-            };
-        };
+
+  Collapsible.inGroup = function (el) {
+    el = el.parent().parent();
+    if (el.hasClass(group.substring(1))) {
+      var grp = $('.' + el.attr('class').split(' ')[1] + ' .open');
+
+      grp.next().toggleClass('hide');
+      grp.children().text(indicators_labels[1]);
+      grp.toggleClass('open');
     };
+  };
 
 
-    Collapsible.inGroup = function($el) {
-        $el = $el.parent().parent();
-        if ($el.hasClass($grp_name.substring(1))) {
-            var grp = $('.' + $el.attr('class').split(' ')[1] + ' .open');
 
-            grp.next().toggleClass('hide');
-            grp.children().text($indi_label[1]);
-            grp.toggleClass('open');
+  for (i = 0; i < $('.collapse li#head').length; i++) {
+    var $label = $('.collapse li#head:eq(' + i + ')');
+
+    if (options.non_js_support === true) {
+      if (!$label.hasClass('open')) {
+        if (!$label.hasClass('hide')) {
+          $label.next().addClass('hide');
         };
+      };
     };
 
 
 
-    for (i = 0; i < $('.collapse li#head').length; i++) {
-        var $label = $('.collapse li#head:eq(' + i + ')');
+    if (options.indicators === true) {
+      var lbl;
+      if ($label.hasClass('open')) {
+        lbl = indicators_labels[0]
+      } else {
+        lbl = indicators_labels[1]
+      };
+      $label.append('<span>' + lbl + '</span>');
+    };
 
-        if (options.safe === true) {
-            if (!$label.hasClass('open')) {
-                if (!$label.hasClass('hide')) {
-                    $label.next().addClass('hide');
-                };
-            };
-        };
-
-
-
-        if (options.indicators === true) {
-            if ($indi_pos == 'left') {
-                $indi_pos = 'style="float:left"';
-            }
+  };
 
 
-            if ($label.hasClass('open')) {
-                $label.append('<span ' + $indi_pos + '>' + $indi_label[0] + '</span>');
-            } else {
-                $label.append('<span ' + $indi_pos + '>' + $indi_label[1] + '</span>');
-            };
+  if (options.rounded == true) {
+    for (i = 0; i < $(group).length; i++) {
+
+      $(group).eq(i).addClass('grp_' + i + '');
 
 
+      $(".grp_" + i + " ul").first().addClass('first');
+      $(".grp_" + i + " ul").last().addClass('last');
+    };
+  }
 
+  $('.collapse li#head').bind(options.event_trigger, function (e) {
 
-        };
+    var el = $(e.target);
 
-};
+    if (el.parent().children().length > 1) {
 
-
-
-for (i = 0; i < $($grp_name).length; i++) {
-    var $thisGroup = $grp_name + ':eq(' + i + ')';
-
-    $($thisGroup).addClass('grp_' + i + '');
-};
-
-
-$('.collapse li#head').bind(options.event, function(e) {
-
-    var $el = $(e.target);
-
-    if ($el.parent().children().length > 1) {
-
-        if ($el.hasClass('open')) {
-            Collapsible.indicators($el);
-        }
-        else {
-            Collapsible.inGroup($el);
-            Collapsible.indicators($el);
-        };
+      if (el.hasClass('open')) {
+        Collapsible.indicators(el);
+      } else {
+        Collapsible.inGroup(el);
+        Collapsible.indicators(el);
+      };
     }
     if (options.auto_scroll === true) {
-        var scroll_top = $el.position().top;
-        $("html, body").animate({
-            scrollTop: scroll_top
-        }, options.auto_scroll_speed);
+      var scroll_top = el.position().top;
+      $("html, body").animate({
+        scrollTop: scroll_top
+      }, options.auto_scroll_speed);
     };
-});
+  });
 };
+
+
+$('label').collapsible({
+  non_js_support: true,
+  event_trigger: 'click',
+  group_name: '.group',
+  indicators: true,
+  auto_scroll: true,
+  auto_scroll_speed: 700,
+  rounded: true
+});
